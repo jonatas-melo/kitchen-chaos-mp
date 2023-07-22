7556afd87442a0126056ff245b40f9f4b1c5595c
Jonatas Melo <jonatas.cmelo@gmail.com>
2023-07-21 20:51:45 -0700

    Sync animations - Client auth

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour {
+public class PlayerAnimator : NetworkBehaviour {
 
 
     private const string IS_WALKING = "IsWalking";
@@ -18,7 +19,13 @@ public class PlayerAnimator : MonoBehaviour {
         animator = GetComponent<Animator>();
     }
 
-    private void Update() {
+    private void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
 
